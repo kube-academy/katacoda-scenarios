e189ae9b19fde7a510e7802c5aedb319b4e4f417
Eitan Suez <sueze@vmware.com>
2021-09-29 12:04:26 -0500

    making deploy.sh a foreground script so user knows what's going on; sometimes using selectors will output nothing because deployment or pods have not yet had time to be created

--- kubectl-intermediate/deploy.sh
+++ kubectl-intermediate/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set +v # suppress echoing the echo command being invoked
+clear
 echo "Please wait a moment while we apply some deployments to the frontend namespace.."
 kubectl create -f ~/label-deploy.yaml
-sleep 5
